520b4ad6d08766e377e8d67fb05662695afab7a3
victorsouza2382 <victorsouza2382@gmail.com>
2020-02-12 20:57:26 -0300

    Atividade 01 concluida

--- Atividades/Atividade01/src/modelo/persistencia/ConexaoBD.java
+++ Atividades/Atividade01/src/modelo/persistencia/ConexaoBD.java
@@ -8,15 +8,15 @@ public class ConexaoBD {
     private static Connection conexao;
 
     public static Connection getConexao()
-            throws DadosExeption {
+            throws DadosException {
         if (conexao == null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
                 conexao = DriverManager.getConnection("jdbc:mysql://localhost/cadastro", "root", "");
             } catch (ClassNotFoundException ex) {
-                throw new DadosExeption("Erro ao carregar JDBC");
+                throw new DadosException("Erro ao carregar JDBC");
             } catch (SQLException ex) {
-                throw new DadosExeption("Erro ao conectar MySQL");
+                throw new DadosException("Erro ao conectar MySQL");
             }
 
         }
