2e7fdf29488e246a102ce8ff29288a6798f2e797
victorsouza2382 <victorsouza2382@gmail.com>
2020-05-13 20:57:21 -0300

    Atividade15

---
+++ Atividade15/src/java/managedbean/ControladorMB.java
@@ -0,0 +1,32 @@
+package managedbean;
+
+import ejb.StatefulEJB;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+/**
+ * @author Victor de Souza
+ */
+
+
+@Named (value = "controladorMB")
+@SessionScoped
+public class ControladorMB implements Serializable{
+    
+    @EJB
+    private StatefulEJB ejb;
+
+    public ControladorMB() {
+        System.out.println("Criando o controlador...");
+    }
+    
+    public StatefulEJB getEjb() {
+        return ejb;
+    }
+
+    public void setEjb(StatefulEJB ejb) {
+        this.ejb = ejb;
+    }
+}
