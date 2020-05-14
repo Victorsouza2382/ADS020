17453b213e1390bb053df8e792286278345a4ce5
victorsouza2382 <victorsouza2382@gmail.com>
2020-05-13 21:44:53 -0300

    Atividade16

---
+++ Atividade16/CarrinhoEJB/src/java/ejb/CarrinhoCompraLocal.java
@@ -0,0 +1,18 @@
+/*
+ * @author Victor de Souza
+ */
+package ejb;
+
+import java.util.List;
+import javax.ejb.EJBException;
+import javax.ejb.Local;
+
+@Local
+public interface CarrinhoCompraLocal {
+    
+    public void adicionarItem(String item) throws EJBException;
+    public List<String> listarItens() throws EJBException;
+
+    void businessMethod();
+    
+}
